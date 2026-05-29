fd863cadb9e55e000bc6f2b81e17a45056a012d5
Abhishek-pratap9120 <abhi911387@gmail.com>
2026-05-29 07:53:25 +0530

    all code add

---
+++ child/azurerm_bastion_Host/main.tf
@@ -0,0 +1,12 @@
+# resource "azurerm_bastion_host" "bastion" {
+#     for_each = var.bastion
+#   name                = each.value.bastion_name
+#   location            = each.value.bastion_location
+#   resource_group_name = each.value.rg_name
+
+#   ip_configuration {
+#     name                 = "configuration"
+#     subnet_id            = data.azurerm_subnet.data_subnet[each.key].id
+#     public_ip_address_id = data.azurerm_public_ip.data_pip[each.key].id
+#   }
+# }
